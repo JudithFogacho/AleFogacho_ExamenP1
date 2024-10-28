2bf4a4bf1ebdf38f946c3048fb4d73f21a6d6225
JudithFogacho <judith.fogacho@udla.edu.ec>
2024-10-28 09:33:20 -0500

    atributos

---
+++ Models/AF_Cita.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AleFogacho_ExamenP1.Models
+{
+    public class AF_Cita
+    {
+        [Key]
+        public int AF_CitaId { get; set; }
+        [Required]
+        public DateTime AF_FechaIngreso { get; set; }
+        [Required]
+        public string? AF_NombreCita { get; set;}
+        [Required]
+        [StringLength(100)]
+        public string? AF_Detalle { get; set;}
+        [Required]
+        [Range (0.01, 100.00)]
+        public decimal AF_ValorCita { get; set;}
+        [Required]
+        public bool AF_Seguro { get; set;}
+
+    }
+}
